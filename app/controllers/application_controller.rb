ab10897d13b52b94f4ff0d805c1b44236523cff1
Ben Ubois <ben@benubois.com>
2024-09-15 11:18:21 +0200

    Config updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_cache_headers, if: -> { request.get? }
+
+  private
+
+  def set_cache_headers
+    expires_in 4.hours, public: true
+  end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -4,10 +4,10 @@ workers 0
 threads_count = ENV.fetch("MAX_THREADS", 4)
 threads threads_count, threads_count
 
-environment ENV.fetch("RACK_ENV", "development")
-
-shared_directory = File.join(File.expand_path("..", ENV["PWD"]), "shared")
-shared_directory = File.directory?(shared_directory) ? shared_directory : ENV["PWD"]
-
-pidfile File.join(shared_directory, "tmp", "puma.pid")
-bind    File.join("unix://", shared_directory, "tmp", "puma.sock")
+if Rails.env.production?
+  pidfile File.join(ENV["SHARED_DIRECTORY"], "tmp", "puma.pid")
+  bind File.join("unix://", ENV["SHARED_DIRECTORY"], "tmp", "puma.sock")
+else
+  port ENV.fetch("PORT", 3000)
+  plugin :tmp_restart
+end
