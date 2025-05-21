5563e9cfd7d4ab057d0f03d759307522d0e53859
Ben Ubois <ben@benubois.com>
2025-05-21 13:22:21 +0200

    Config update.

--- config/puma.rb
+++ config/puma.rb
@@ -3,9 +3,9 @@ workers 0
 threads_count = ENV.fetch("MAX_THREADS", 4)
 threads threads_count, threads_count
 
-if ENV.fetch("RAILS_ENV") == "production"
-  pidfile File.join(ENV["SHARED_DIRECTORY"], "tmp", "puma.pid")
-  bind File.join("unix://", ENV["SHARED_DIRECTORY"], "tmp", "puma.sock")
+if ENV["PUMA_SOCKET"] && ENV["PUMA_PID"]
+  bind ENV["PUMA_SOCKET"]
+  pidfile ENV["PUMA_PID"]
 else
   port ENV.fetch("PORT", 3000)
   plugin :tmp_restart
